9aad2840a2c9bfa9988eaa067369dd4a84e747ec
IvailoB <102227501+IvailoB@users.noreply.github.com>
2023-12-17 12:03:44 +0200

    fix the bug with test dependency

---
+++ Employee-Project/src/main/java/com/Project/EmployeeProject/EmployeeProjectApplication.java
@@ -0,0 +1,41 @@
+package com.Project.EmployeeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
+
+@SpringBootApplication
+public class EmployeeProjectApplication {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "123456");
+			statement = connection.createStatement();
+			statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname = 'employeeProject'");
+			ResultSet resultSet = statement.getResultSet();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			if (count <= 0) {
+				statement.executeUpdate("CREATE DATABASE employeeProject");
+			}
+		} catch (SQLException e) {
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+			}
+		}
+
+		SpringApplication.run(EmployeeProjectApplication.class, args);
+	}
+
+}
