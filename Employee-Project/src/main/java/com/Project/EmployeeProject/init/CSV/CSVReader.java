89e096fdb175950a676629cfba4665d9d5e8725e
IvailoB <102227501+IvailoB@users.noreply.github.com>
2023-12-20 18:40:24 +0200

    fix date.csv and CSVReader

--- Employee-Project/src/main/java/com/Project/EmployeeProject/init/CSV/CSVReader.java
+++ Employee-Project/src/main/java/com/Project/EmployeeProject/init/CSV/CSVReader.java
@@ -1,5 +1,6 @@
 package com.Project.EmployeeProject.init.CSV;
 
+import com.Project.EmployeeProject.models.entity.Date;
 import com.Project.EmployeeProject.models.entity.Employee;
 import com.Project.EmployeeProject.models.entity.Project;
 import com.Project.EmployeeProject.services.EmployeeService;
@@ -11,6 +12,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 @Component
@@ -35,17 +37,26 @@ public class CSVReader {
                 Long userId = Long.parseLong(values[0]);
                 Long projectId = Long.parseLong(values[1]);
                 LocalDate startDate = LocalDate.parse(values[2]);
-                LocalDate dateTo = "null".equalsIgnoreCase(values[3]) ? null : LocalDate.parse(values[3], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                LocalDate endDate = "null".equalsIgnoreCase(values[3]) ? null : LocalDate.parse(values[3], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-                Employee employee = getEmployee(userId, startDate, dateTo);
+                Employee employee = getEmployee(userId);
                 Project project = getProject(projectId);
+                Date date = getDate(startDate, endDate);
 
+                employee.getDates().add(date);
                 employee.getProjects().add(project);
                 employeeService.init(employee);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Date getDate(LocalDate startDate, LocalDate endDate) {
+        Date date = new Date();
+        date.setEndDate(endDate);
+        date.setStartDate(startDate);
+        return date;
     }
 
     private Project getProject(Long projectId) {
@@ -58,14 +69,13 @@ public class CSVReader {
                 });
     }
 
-    private Employee getEmployee(Long userId, LocalDate startDate, LocalDate dateTo) {
+    private Employee getEmployee(Long userId) {
         return employeeService.findById(userId)
                 .orElseGet(() -> {
                     Employee newEmployee = new Employee();
                     newEmployee.setId(userId);
-                    newEmployee.setStartDate(startDate);
-                    newEmployee.setEndDate(dateTo);
                     newEmployee.setProjects(new HashSet<>());
+                    newEmployee.setDates(new ArrayList<>());
                     return newEmployee;
                 });
     }
